5ef3d6749de21bdadd5e52ee5b94a5f8987639f0
devadob <admir.obralija@tik.uni-stuttgart.de>
2024-07-10 11:39:18 +0200

    Implement event listeners to save all unsaved changes.
    - Bump version from 0.1.0 to 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,37 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension for integrated Jupyter operation.',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab-integration-extension is activated!');
+    const { commands } = app;
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // In critical cases Jupyter Notebooks and Jupyterlab projects should be saved when the page/frame is closed or left.
+    if (urlParams.has('saveBeforeUnload')) {
+      window.addEventListener("beforeunload", (evt) => {
+        console.log("Saving all unsaved changes...");
+        commands.execute("docmanager:save-all");
+        // Prevent further 'unsaved changes' warnings in Jupyter Notebooks.
+        evt.stopImmediatePropagation();
+      });
+      console.log("Integrated saving before unload is activated.");
+    }
+
+    // In case of previewing a Jupyter Notebook or a Jupyterlab project and making some experimental changes,
+    // no 'unsaved changes' warnings should be shown when the preview is closed or left.
+    if (urlParams.has('preventBeforeUnloadPropagation')) {
+      window.addEventListener("beforeunload", (evt) => {
+        evt.stopImmediatePropagation();
+      });
+    }
+
+    // MessageEvent listener for explicit saving of a Jupyter Notebook or a Jupyterlab project.
+    // This might be used, e.g., on form submissions.
+    window.addEventListener("message", (evt) => {
+      if (evt.data == "save-all") {
+        console.log("Saving all unsaved changes...");
+        commands.execute("docmanager:save-all");
+      }
+    });
   }
 };
 
